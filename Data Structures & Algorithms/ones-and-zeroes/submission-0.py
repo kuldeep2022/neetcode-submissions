7ee7cec60bbc7c4beb8fc8475e17e654b61cfdca
kuldeep2022 <103405033+kuldeep2022@users.noreply.github.com>
2026-08-17 19:19:53 -0700

    Add: ones-and-zeroes - submission-0

---
+++ Data Structures & Algorithms/ones-and-zeroes/submission-0.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = {}
+        def dfs(i,m,n):
+            if i == len(strs):
+                return 0
+            
+            if (i,m,n) in dp:
+                return dp[(i,m,n)]
+            
+            mCnt,nCnt = strs[i].count("0"), strs[i].count("1")
+            dp[(i,m,n)] = dfs(i+1,m,n)
+            if mCnt <= m and nCnt <= n:
+                dp[(i,m,n)] = max(dp[(i,m,n)], 1 + dfs(i+1, m-mCnt, n-nCnt))
+            
+            return dp[(i,m,n)]
+        
+        return dfs(0,m,n)
+
+
+        arr = [[0] * 2 for _ in range(len(strs))]
+        for i, s in enumerate(strs):
+            for c in s:
+                arr[i][ord(c) - ord('0')] += 1
+
+        print(arr)
+        dp = {}
+
+        def dfs(i, m, n):
+            if i == len(strs):
+                return 0
+            if m == 0 and n == 0:
+                return 0
+            if (i, m, n) in dp:
+                return dp[(i, m, n)]
+
+            res = dfs(i + 1, m, n)
+            if m >= arr[i][0] and n >= arr[i][1]:
+                res = max(res, 1 + dfs(i + 1, m - arr[i][0], n - arr[i][1]))
+            dp[(i, m, n)] = res
+            return res
+
+        return dfs(0, m, n)
